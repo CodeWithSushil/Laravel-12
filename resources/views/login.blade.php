c0f2cfe3efa35f8eae778eddebe8b165ba41d125
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-03-25 21:48:23 +0530

    packages update

---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<div>
+   <h2> Login</h2>
+   <form method="GET" action="#">
+      <input type="search" name="s" placeholder="Search...">
+      <button type="submit">Login</button>
+   </form>
+</div>
